313bda7b3b28f10cd7efd21d0456a5752ab19a21
theunsignedunderground <editor@theunsignedunderground.com>
2025-09-08 13:16:08 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 // pages/_app.js
+import { ClerkProvider } from "@clerk/nextjs";
 import Header from "../components/Header";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </ClerkProvider>
   );
 }

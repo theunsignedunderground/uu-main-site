8908f45cfaab4fd80ffc9cf54d08a2ada72a2b5e
theunsignedunderground <editor@theunsignedunderground.com>
2025-09-15 13:29:00 -0500

    Create colors.js

---
+++ styles/colors.js
@@ -0,0 +1,9 @@
+// styles/colors.js
+const colors = {
+  outlawRed: "#871F1A",
+  vintageCream: "#F4E6D0",
+  black: "#1C1C1C",
+};
+
+export default colors;
+export const { outlawRed, vintageCream, black } = colors;
